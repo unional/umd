c3a03df59d27f06f508f0cb7e05a0d2fbe42b81b
Homa Wong <hwong@paloaltonetworks.com>
2014-09-27 16:54:54 -0700

    Bump version to 0.4.7

--- umd.js
+++ umd.js
@@ -1,7 +1,7 @@
 /*
  * Copyright(c) 2014, Unional (https://github.com/unional)
  * @license Licensed under the MIT License (https://github.com/unional/unional/LICENSE)).
- * @version 0.4.6
+ * @version 0.4.7
  * Created by unional on 9/21/14.
  */
 //noinspection ThisExpressionReferencesGlobalObjectJS
